67912ca5830e290c8dfd957a9bfe9534a2de8add
shree <shreegopal2390@gmail.com>
2023-09-29 10:35:45 +0530

    first commit

---
+++ Tasky/shared/Tasky.DbMigrator/TaskyDbMigratorModule.cs
@@ -0,0 +1,31 @@
+﻿using Tasky.AdministrationService;
+using Tasky.AdministrationService.EntityFrameworkCore;
+using Tasky.IdentityService;
+using Tasky.IdentityService.EntityFrameworkCore;
+using Tasky.ProjectService;
+using Tasky.ProjectService.EntityFrameworkCore;
+using Tasky.SaaSService;
+using Tasky.SaaSService.EntityFrameworkCore;
+using Volo.Abp.Autofac;
+using Volo.Abp.Modularity;
+
+namespace Tasky.DbMigrator;
+
+[DependsOn(
+    typeof(AbpAutofacModule),
+    typeof(AdministrationServiceEntityFrameworkCoreModule),
+    typeof(AdministrationServiceApplicationContractsModule),
+    typeof(IdentityServiceEntityFrameworkCoreModule),
+    typeof(IdentityServiceApplicationContractsModule),
+    typeof(SaaSServiceEntityFrameworkCoreModule),
+    typeof(SaaSServiceApplicationContractsModule),
+    typeof(ProjectServiceEntityFrameworkCoreModule),
+    typeof(ProjectServiceApplicationContractsModule)
+    )]
+public class TaskyDbMigratorModule : AbpModule
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        //Configure<AbpBackgroundJobOptions>(options => options.IsJobExecutionEnabled = false);
+    }
+}
